6aa638cf915e01a2c67b79ebe6552bf4fe6e0ac8
vanetachu <vanessa.mendozavargas@gmail.com>
2025-01-30 21:37:28 -0500

    primera clase

---
+++ python_paltita.py
@@ -0,0 +1,11 @@
+nombre_de_variable = 2 * 8 #aqui estoy multiplicando 2 enteros
+print(nombre_de_variable)
+#print("ñ")
+# print("hola_mundo")
+# print("hola_mundo")
+primer_nombre = "vanessa"
+apellido_paterno = "mendoza"
+espacio = "-"
+print(primer_nombre + espacio + apellido_paterno)
+print(f"mi nombre es: {primer_nombre} {apellido_paterno}")
+print(f"hay {nombre_de_variable} huevos en la canasta")
